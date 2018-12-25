df81fd283cbecf65c337d8a1b6baa1cf42e20a5a
ey3leaf <ey3leaf@gmail.com>
2018-12-25 09:58:37 +0200

    Update install_ruby.sh

--- ruby-config-scripts/install_ruby.sh
+++ ruby-config-scripts/install_ruby.sh
@@ -1,6 +1,6 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+source /usr/local/rvm/scripts/rvm
 rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
